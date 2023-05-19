af0098492366f81679e786fc5c14db667c618336
BerrieV1 <114003039+BerrieV1@users.noreply.github.com>
2023-05-19 16:40:38 +0200

    update play_sound

--- scripts/play_sound.py
+++ scripts/play_sound.py
@@ -1,15 +1,11 @@
 import subprocess
 from basemodule import BaseModule
 from playsound import playsound
-from datetime import datetime
 
 
 class Play_sound(BaseModule):
     def run(self):
         playsound("../sounds/suprise.mp3")
-        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        with open("../sounds/logs.txt", "a") as sounds_file:
-            sounds_file.write(f"Sound played at {current_time}\n")
 
     def install_requirements(self):
         libraries = ["playsound==1.2.2"]
