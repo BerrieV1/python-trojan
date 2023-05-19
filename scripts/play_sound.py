f2ba77fe7237ac9e559ff0bbd5df5cd4a3308a75
BerrieV1 <114003039+BerrieV1@users.noreply.github.com>
2023-05-19 14:15:23 +0200

    fix sound module name

--- scripts/play_sound.py
+++ scripts/play_sound.py
@@ -12,6 +12,6 @@ class Play_sound(BaseModule):
             sounds_file.write(f"Sound played at {current_time}\n")
 
     def install_requirements(self):
-        libraries = ["playsound"]
+        libraries = ["playsound==1.2.2"]
         for library in libraries:
             subprocess.check_call(["pip", "install", library])
