46c1be5e0d74b6d3b42a3ae423351febcdd9fbcb
BerrieV1 <114003039+BerrieV1@users.noreply.github.com>
2023-05-19 14:21:14 +0200

    fix sound module

--- scripts/play_sound.py
+++ scripts/play_sound.py
@@ -1,4 +1,3 @@
-import os
 import subprocess
 from basemodule import BaseModule
 from playsound import playsound
@@ -7,13 +6,12 @@ from datetime import datetime
 
 class Play_sound(BaseModule):
     def run(self):
-        audio_file = os.path.dirname(__file__) + '/suprise.mp3'
-        playsound(audio_file)
+        playsound("../sounds/suprise.mp3")
         current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         with open("../sounds/logs.txt", "a") as sounds_file:
             sounds_file.write(f"Sound played at {current_time}\n")
 
     def install_requirements(self):
-        libraries = ["playsound"]
+        libraries = ["playsound=1.2.2"]
         for library in libraries:
             subprocess.check_call(["pip", "install", library])
